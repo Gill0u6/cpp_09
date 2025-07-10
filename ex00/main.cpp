d22b5c346d658fd004e8600a27819c49770f7152
Alex Gilles <agilles@c2r4p1.42lehavre.fr>
2025-07-10 17:19:06 +0200

    setInput valid need parsing (Makefile Flags modif for test)

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 17:48:10 by agilles           #+#    #+#             */
-/*   Updated: 2025/07/02 17:50:07 by agilles          ###   ########.fr       */
+/*   Updated: 2025/07/10 16:48:54 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	main(int ac, char **av)
 {
+	(void)av;
 	if (ac != 2)
 	{
 		std::cerr << "Error: Wrong Argument count: " << std::endl;
 		std::cerr << "\t You should call the progam like this: ./btc input.txt" << std::endl;
 		return (1);
 	}
+	BitcoinExchange btc;
 	return (0);
 }
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 17:29:08 by agilles           #+#    #+#             */
-/*   Updated: 2025/07/02 18:24:03 by agilles          ###   ########.fr       */
+/*   Updated: 2025/07/10 17:17:32 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 BitcoinExchange::BitcoinExchange()
 {
 	std::cout << "bitcoinExchange Default constructor called" << std::endl;
+	this->setInput("input.txt");
 }
 
 BitcoinExchange::BitcoinExchange(std::string csv, std::string input)
@@ -24,6 +25,7 @@ BitcoinExchange::BitcoinExchange(std::string csv, std::string input)
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &cp)
 {
+	(void)cp;
 	std::cout << "bitcoinExchange Copy constructor called" << std::endl;
 }
 
@@ -34,6 +36,7 @@ BitcoinExchange::~BitcoinExchange()
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &cp)
 {
+	(void)cp;
 	std::cout << "bitcoinExchange Assignation operator called" << std::endl;
 }
 
@@ -75,10 +78,10 @@ void	BitcoinExchange::setData()
 
 void	BitcoinExchange::setInput(std::string input)
 {
-	std::ifstream file("../data.csv");
+	std::ifstream file(input);
 	if (!file.is_open())
 	{
-		std::cerr << "Error: fail when open data.csv" << std::endl;
+		std::cerr << "Error: fail when open input file" << std::endl;
 		return ;
 	}
 
@@ -87,24 +90,29 @@ void	BitcoinExchange::setInput(std::string input)
 	while (std::getline(file, line))
 	{
 		std::istringstream ss(line);
+		std::string totrim;
 		std::string date;
 		std::string btc;
 
-		if (!std::getline(ss, date, ','))
+		if (!std::getline(ss, totrim, '\n'))
 			continue ;
-
-		if (!std::getline(ss, btc))
-			continue ;
-
-		try
+		if (totrim.size() > 13)
 		{
-			float	btc_convert = std::stof(btc);
-			this->_data[date] = btc_convert;
+			date = totrim.substr(0, 10);
+			btc = totrim.substr(13);
+			try
+			{
+				float	btc_convert = std::stof(btc);
+				std::cout << date << " " << btc << std::endl;
+				this->_data[date] = btc_convert;
+			}
+			catch (const std::exception &e)
+			{
+				std::cerr << "Error when convert float in this line: \n" << line << std::endl;
+			}
 		}
-		catch (const std::exception &e)
-		{
-			std::cerr << "Error when convert float in this line: \n" << line << std::endl;
-		}
+		else
+			std::cerr << "Invalid Input" << std::endl;
 	}
 	file.close();
 }
